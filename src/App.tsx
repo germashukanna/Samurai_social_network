ae03e05b658f074589a9b07c7ac116419b5e3cf4
germashukanna <50459965+germashukanna@users.noreply.github.com>
2022-05-25 21:51:51 +0300

    Revert "69. Hoc 2.0"
    This reverts commit ae4864b46bbe91df7204722c5c2debaa49b57260.

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Settings from "./components/Settings/Settings";
-import Music from "./components/Music/Music";
-import New from "./components/New/New";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Navbar from "./components/Navbar/Navbar";
-import {SuperDialogsContainer} from "./components/Dialogs/DialogsContainer";
-import {ProfileContainerToStore} from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import {Login} from "./components/Login/Login";
-import {ReduxStoreType} from "./redux/redux-store";
-import {UsersContainerToStore} from "./components/Users/UsersContainer";
 
-
-
-type AppPropsType = {
-    store: ReduxStoreType
-
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-const App: React.FC<AppPropsType> = (props) => {
-    const state = props.store.getState()
-
-    return (
-        <BrowserRouter>
-            <div className={'app-wrapper'}>
-                <HeaderContainer/>
-                <Navbar state={state.sidebarPage}/>
-                <div className={'.app-wrapper-content'}>
-                    <Routes>
-                        <Route path={"/dialogs"}
-                               element={<SuperDialogsContainer/>}/>
-                        <Route path={"/profile"}
-                               element={<ProfileContainerToStore/>}/>
-                        <Route path={"/profile/:userId?"}
-                               element={<ProfileContainerToStore/>}/>
-                        <Route path={"/new"} element={<New/>}/>
-                        <Route path={"/Music"} element={<Music/>}/>
-                        <Route path={"/settings"} element={<Settings/>}/>
-                        <Route path={"/users"} element={<UsersContainerToStore/>}/>
-                        <Route path={"/login"} element={<Login/>}/>
-                        {/*<Route path={"/sidebar"} element={<SidebarItem key={1} img={""} name={""}/>}*/}
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
-    )
-}
 export default App;
