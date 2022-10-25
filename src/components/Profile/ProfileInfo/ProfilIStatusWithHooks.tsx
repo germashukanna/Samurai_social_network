0b1ce42dbe8f427fe052da894e7040381cc8aa2f
germashukanna <50459965+germashukanna@users.noreply.github.com>
2022-10-25 19:36:37 +0300

    85 - hook, useEffect, хуки

--- src/components/Profile/ProfileInfo/ProfilIStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfilIStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 
 type ProfileStatusType = {
@@ -10,6 +10,10 @@ export const ProfilIStatusWithHook: React.FC<ProfileStatusType> = (props) => {
 
     const [editMode, setEditMode] = useState<boolean>(false)
     const [status, setStatus] = useState<string>(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMode(true)
