86a0419a99844c4884328d581f832e943b73d6f1
germashukanna <germashuk@gmail.com>
2022-12-10 14:11:33 +0300

    Updates

--- src/components/common/Preloader/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import preloader from "../../../assets/images/preloader.svg";
-import React from "react";
-
-
-export const Preloader = () => {
-  return(
-      <div style={{backgroundColor: "white"}}><img src={preloader} /></div>
-  )
-}
--- src/redux/Hooks.ts
+++
@@ -1,6 +0,0 @@
-import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
-import {AppDispatch, AppStateType} from "./redux-store";
-
-
-export const useAppDispatch = () => useDispatch<AppDispatch>()
-export const useAppSelector: TypedUseSelectorHook<AppStateType> = useSelector
